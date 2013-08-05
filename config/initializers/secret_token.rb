5c9cd0a35f7c4a8b7de0dfce557ffc55ce85e5d3
Steven D. Majewski <steve.majewski@gmail.com>
2013-08-05 13:50:27 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sufia::Application.config.secret_key_base = '50dc9fb7a53d66bda284c4d94d8e330f9ab51c39346a7c2a59de705aedc3a77fb8db10692d7fb5037155d0d37fd8a9fdd6f0dc033abc5d7a77fe2f11d9c95990'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sufia::Application.config.session_store :cookie_store, key: '_sufia_session'
